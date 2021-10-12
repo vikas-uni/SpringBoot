1086be5e2218dd181a8596f4ae6f2ea1fc5d9afb
Vikas <vikas.uni@gmail.com>
2021-10-12 09:45:06 +0530

    add spring security

---
+++ SpringBootKafka/src/main/java/demo/security/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Create 2 users for demo
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER")
+                .and()
+                .withUser("admin").password("password").roles("USER", "ADMIN");
+
+    }
+
+    // Secure the endpoins with HTTP Basic authentication
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                //HTTP Basic authentication
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/javainuse-kafka/producer/**").hasRole("USER")
+                .antMatchers(HttpMethod.POST, "/javainuse-kafka/producer").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/javainuse-kafka/producer/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/javainuse-kafka/producer/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/javainuse-kafka/producer/**").hasRole("ADMIN")
+                .and()
+                .csrf().disable()
+                .formLogin().disable();
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailsService() {
+        //ok for demo
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(users.username("user").password("password").roles("USER").build());
+        manager.createUser(users.username("admin").password("password").roles("USER", "ADMIN").build());
+        return manager;
+    }*/
+
+}
