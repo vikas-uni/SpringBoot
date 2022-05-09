6cbd374587898823f533ad76d4e09c66cbf46cef
Vikas <vikas.uni@gmail.com>
2022-05-09 16:38:20 +0530

    misc changes

--- MyBootOauthImpl/src/main/java/com/boot/security/springSecurityConfig/SpringSecurityConfig.java
+++ MyBootOauthImpl/src/main/java/com/boot/security/springSecurityConfig/SpringSecurityConfig.java
@@ -1,12 +1,8 @@
 package com.boot.security.springSecurityConfig;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
