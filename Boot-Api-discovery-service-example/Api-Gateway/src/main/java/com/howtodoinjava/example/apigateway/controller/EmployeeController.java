c72f73eea259f15de01f97510f5a4e6acd8fb585
Vikas <vikas.uni@gmail.com>
2022-06-07 10:25:43 +0530

    adding zuul proxy

--- Boot-Api-discovery-service-example/Api-Gateway/src/main/java/com/howtodoinjava/example/apigateway/controller/EmployeeController.java
+++ Boot-Api-discovery-service-example/Api-Gateway/src/main/java/com/howtodoinjava/example/apigateway/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.howtodoinjava.example.apigateway.controller;
 
-import java.util.concurrent.CompletableFuture;
+import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -11,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -20,9 +22,12 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class EmployeeController {
 
 	Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
-	
+
 	@Autowired
 	RestTemplate restTemplate;
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
 
 	@RequestMapping(value = "/employeeDetails/{employeeid}", method = RequestMethod.GET)
 	@HystrixCommand(fallbackMethod = "fallbackMethod")
@@ -43,27 +48,52 @@ public class EmployeeController {
 	public String testMethod(@PathVariable String testStr) {
 		LOGGER.info("Getting Employee details for " + testStr);
 
-		String response = restTemplate.exchange("http://employee-service/getTestString/{employeeId}",
-				HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+		String response = restTemplate.exchange("http://employee-service/getTestString/{employeeId}", HttpMethod.GET,
+				null, new ParameterizedTypeReference<String>() {
 				}, testStr).getBody();
 
 		LOGGER.info("Response Body " + response);
 
 		return "Employee Id -  " + testStr + " [ Employee Details " + response + " ]";
 	}
-	
+
 	@RequestMapping(value = "/testAsync/{employeeId}", method = RequestMethod.GET)
 	@HystrixCommand(fallbackMethod = "asyncFallbackMethod")
-    public ResponseEntity<Object> testAsync (@PathVariable String employeeId) throws InterruptedException {
+	// , commandProperties = { @HystrixProperty(name
+	// ="execution.isolation.thread.timeoutInMilliSeconds", value = "5000")})
+	public ResponseEntity<Object> testAsync(@PathVariable String employeeId) throws InterruptedException {
 		LOGGER.info("in testAsync");
-		String response = restTemplate.exchange("http://employee-service/testAsync/{employeeId}",
-				HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+		String response = restTemplate.exchange("http://employee-service/testAsync/{employeeId}", HttpMethod.GET, null,
+				new ParameterizedTypeReference<String>() {
 				}, employeeId).getBody();
 
 		LOGGER.info("got result testAsync Response Body " + response);
-
 		return ResponseEntity.ok().body(response);
-    }
+	}
+
+	//currently this is not woring
+	@RequestMapping(value = "/testzuul/employeeDetails/{employeeid}", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "fallbackMethod")
+	public String testzuul(@PathVariable int employeeid) {
+		LOGGER.info("Getting Employee details by zuul " + employeeid);
+
+		List<ServiceInstance> instances = discoveryClient.getInstances("employee-zuul-service");
+		ServiceInstance serviceInstance = instances.get(0);
+
+		String baseUrl = serviceInstance.getUri().toString();
+
+		baseUrl = baseUrl + "/api/demo/findEmployeeDetails/{employeeid}";
+		
+		LOGGER.info("baseUrl baseUrl " + baseUrl);
+
+		String response = restTemplate
+				.exchange(baseUrl, HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+				}, employeeid).getBody();
+
+		LOGGER.info("Response Body " + response);
+
+		return "Employee Id -  " + employeeid + " [ Employee Details " + response + " ]";
+	}
 
 	public String fallbackMethod(int employeeid) {
 
@@ -74,15 +104,16 @@ public class EmployeeController {
 
 		return "Fallback response:: No details available temporarily " + myStr;
 	}
-	
+
 	public ResponseEntity<Object> asyncFallbackMethod(String myStr) {
 
 		return ResponseEntity.ok("Fallback response asyncFallbackMethod:: No details available temporarily " + myStr);
 	}
 
-	/*@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}*/
+	/*
+	 * @Bean
+	 * 
+	 * @LoadBalanced public RestTemplate restTemplate() { return new RestTemplate();
+	 * }
+	 */
 }
