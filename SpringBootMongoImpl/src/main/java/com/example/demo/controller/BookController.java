2a341fbb41005a87c0691ff5ba118639eeffe752
Vikas Gond <vikas.gond@nagarro.com>
2025-09-08 13:53:39 +0530

    adding logger

--- SpringBootMongoImpl/src/main/java/com/example/demo/controller/BookController.java
+++ SpringBootMongoImpl/src/main/java/com/example/demo/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,26 +13,27 @@ import java.util.Optional;
   
 @RestController
 public class BookController {
-  
+	private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+	
     @Autowired
     private BookRepo repo;
   
     @PostMapping("/addBook")
     public String saveBook(@RequestBody Book book){
         repo.save(book);
-        
+        logger.info("adding book: {}", book);
         return "Added Successfully";
     }
   
     @GetMapping("/findAllBooks")
     public List<Book> getBooks() {
-        
+        logger.info("finding all books...");
         return repo.findAll();
     }
   
     @GetMapping("/getBook/{id}")
     public Optional<Book> getBook(@PathVariable int id) {
-        
+    	logger.info("finding book by id: {}", id);
         return repo.findById(id);
     }
   
@@ -38,7 +41,7 @@ public class BookController {
     @DeleteMapping("/delete/{id}")
     public String deleteBook(@PathVariable int id){
         repo.deleteById(id);
-        
+        logger.info("deleting book by id: {}", id);
         return "Deleted Successfully";
     }
   
